5d85d1c1b107c0305f803f2f0de954d8d3ae528b
nickdevis <a2_04@mail.ru>
2013-11-09 02:23:55 +0200

    first upload open cart

---
+++ public/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://bambinotoys/admin/');
+define('HTTP_CATALOG', 'http://bambinotoys/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://bambinotoys/admin/');
+define('HTTPS_CATALOG', 'http://bambinotoys/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\Winginx\home\bambinotoys\public/admin/');
+define('DIR_SYSTEM', 'C:\Winginx\home\bambinotoys\public/system/');
+define('DIR_DATABASE', 'C:\Winginx\home\bambinotoys\public/system/database/');
+define('DIR_LANGUAGE', 'C:\Winginx\home\bambinotoys\public/admin/language/');
+define('DIR_TEMPLATE', 'C:\Winginx\home\bambinotoys\public/admin/view/template/');
+define('DIR_CONFIG', 'C:\Winginx\home\bambinotoys\public/system/config/');
+define('DIR_IMAGE', 'C:\Winginx\home\bambinotoys\public/image/');
+define('DIR_CACHE', 'C:\Winginx\home\bambinotoys\public/system/cache/');
+define('DIR_DOWNLOAD', 'C:\Winginx\home\bambinotoys\public/download/');
+define('DIR_LOGS', 'C:\Winginx\home\bambinotoys\public/system/logs/');
+define('DIR_CATALOG', 'C:\Winginx\home\bambinotoys\public/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'babmbino');
+define('DB_PASSWORD', 'ZbTPsQ75jF8nJuTN');
+define('DB_DATABASE', 'babmbino');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ public/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://bambinotoys/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://bambinotoys/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\Winginx\home\bambinotoys\public/catalog/');
+define('DIR_SYSTEM', 'C:\Winginx\home\bambinotoys\public/system/');
+define('DIR_DATABASE', 'C:\Winginx\home\bambinotoys\public/system/database/');
+define('DIR_LANGUAGE', 'C:\Winginx\home\bambinotoys\public/catalog/language/');
+define('DIR_TEMPLATE', 'C:\Winginx\home\bambinotoys\public/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\Winginx\home\bambinotoys\public/system/config/');
+define('DIR_IMAGE', 'C:\Winginx\home\bambinotoys\public/image/');
+define('DIR_CACHE', 'C:\Winginx\home\bambinotoys\public/system/cache/');
+define('DIR_DOWNLOAD', 'C:\Winginx\home\bambinotoys\public/download/');
+define('DIR_LOGS', 'C:\Winginx\home\bambinotoys\public/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'babmbino');
+define('DB_PASSWORD', 'ZbTPsQ75jF8nJuTN');
+define('DB_DATABASE', 'babmbino');
+define('DB_PREFIX', 'oc_');
+?>
